8ffaab5b6e4bb7e2e874a27c09e90f1b5ee656b8
Daniel Haehn <haehn@seas.harvard.edu>
2018-12-05 20:07:45 -0500

    New tensorflow.js models.

--- nets/trainer.py
+++ nets/trainer.py
@@ -1,7 +1,14 @@
 class Trainer():
 
-  def __init__():
+  def __init__(model):
+
+    self.model = model
 
     self.learningrate = 0.001
     self.momentum = 0.9
     self.weightdecay = 0.0004
+
+    self.optimizer = 'sgd'
+    self.loss = 'crossentropy'
+    
+  def 
--- tensorflow.js/scripts/convertmodels.py
+++ tensorflow.js/scripts/convertmodels.py
@@ -1,12 +1,23 @@
+# import keras
+# import tensorflowjs as tfjs
+# from keras.models import load_model
+
+# models = ['01_noise.h5', '02_noise.h5', '03_noise.h5', '04_noise.h5']
+
+# for m in models:
+#   model = load_model(m)
+#   tfjs.converters.save_keras_model(model, 'web/'+m.split('_')[0])
+#   print 'converted', m
+
+
 import keras
 import tensorflowjs as tfjs
 from keras.models import load_model
 
-models = ['01_noise.h5', '02_noise.h5', '03_noise.h5', '04_noise.h5']
+models = ['models/VGG19_length_variation_2/01_noise.h5']
 
 for m in models:
   model = load_model(m)
-  tfjs.converters.save_keras_model(model, 'web/'+m.split('_')[0])
+  tfjs.converters.save_keras_model(model, 'web/length/'+m.split('_')[0])
   print 'converted', m
-
 
